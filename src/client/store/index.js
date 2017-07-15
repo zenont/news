0ce0a753fe2a0a4da434339f78c915f01c539538
Zenon Trujillo <zenon.t@outlook.com>
2017-07-15 16:42:01 -0700

    adding redux observable

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -3,5 +3,4 @@ import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
 const middleware = applyMiddleware(thunk)
-console.log('reducer!!!!!!>>>>>>>>', rootReducer)
 export default createStore(rootReducer, middleware)
