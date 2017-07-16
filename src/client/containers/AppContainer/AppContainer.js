c619653458ed9511f7f8c05fdebf8fca30027bb0
Zenon Trujillo <zenon.t@outlook.com>
2017-07-15 21:57:50 -0700

    updated containers

--- src/client/containers/AppContainer/AppContainer.js
+++ src/client/containers/AppContainer/AppContainer.js
@@ -4,25 +4,19 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
-	Link
 } from 'react-router-dom'
+import ListContainer from './ListContainer'
+import DetailContainer from './DetailContainer'
 
 export class AppContainer extends Component {
 	render() {
 		return (
 			<Router>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/about">About</Link></li>
-					<li><Link to="/topics">Topics</Link></li>
-				</ul>
-				<hr />
 				<div>
 					This is an AppContainer
 					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route path="/about" component={About} />
-						<Route path="/topics" component={Topics} />
+						<Route exact path="/" component={ListContainer} />
+						<Route path="/:articleId" component={DetailContainer} />
 					</Switch>
 				</div>
 			</Router>
--- src/client/containers/ListContainer/ListContainer.js
+++ src/client/containers/ListContainer/ListContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class ListContainer extends Component {
-
 	render() {
 		return (
 			<div>
