44212b44863c2549c6c1334e822a035863f99a19
Zenon Trujillo <zenon.t@outlook.com>
2017-07-25 22:41:43 -0700

    ssr middleware

--- src/server/middlewares/ssr.js
+++ src/server/middlewares/ssr.js
@@ -1,10 +1,10 @@
-import { Page } from './components'
-import RootContainer from '../shared/containers'
-import store from '../shared/store'
+import { Page } from '../components'
+import RootContainer from '../../shared/containers'
+import store from '../../shared/store'
 import { renderToStaticMarkup } from 'react-dom/server'
 
 const middleware = options => {
-	const { redirectCode = 302 } = options;
+	const { redirectCode = 302 } = options
 
 	return (request, response, next) => {
 		const context = {}
@@ -14,46 +14,20 @@ const middleware = options => {
 					server
 					store={store}
 					context={context}
-					location={req.url}
+					location={request.url}
 				/>
 			</Page>
 		)
 
 		if (context.url) {
-			res.writeHead(redirectCode, {
+			response.writeHead(redirectCode, {
 				Location: context.url
 			})
-			res.end()
+			response.end()
 		} else {
-			res.send(stringifiedHtml)
+			response.send(stringifiedHtml)
 		}
 	}
 }
 
-
-app.get('*', (req, res) => {
-	/*fs.readFile(path.join('./', assetsPath, 'index.html'), 'utf8', (err, data) => {
-		console.log('assetsPath', assetsPath)
-		if (err) {
-			return console.log(err)
-		}
-
-		console.log(data)
-	})*/
-
-	console.log('requesting on', req.url, req.params, req.query)
-	const context = {}
-	const stringifiedHtml = renderToStaticMarkup(
-		<Page>
-			<RootContainer
-				server
-				store={store}
-				context={context}
-				location={req.url}
-			/>
-		</Page>
-	)
-	res.send(stringifiedHtml)
-})
-
-export default
+export default middleware
