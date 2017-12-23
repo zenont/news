b84f4500685e5c95352f1d9a7c5b235a1d579f85
Zenon Trujillo <zenontrujillo@zenon-macbook.local>
2017-12-22 21:09:54 -0800

    cleanup

--- src/universal/ajax/__tests__/url.spec.ts
+++ src/universal/ajax/__tests__/url.spec.ts
@@ -2,7 +2,6 @@ import { Url } from '../url'
 
 describe('Url', () => {
 	it('should stringify', () => {
-		// https://newsapi.org/v2/sources?language=en&country=us&apiKey=12345
 		const url = Url.of('https://newsapi.org/v2/')
 			.route('sources')
 			.query({ language: 'en', country: 'us', apiKey: '12345' })
