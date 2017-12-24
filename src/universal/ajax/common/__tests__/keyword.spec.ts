ca4d0d370412068416a6a1ce38e3aea6512f0252
Zenon Trujillo <zenontrujillo@zenon-macbook.local>
2017-12-23 17:23:54 -0800

    testing grouping of keywords

--- src/universal/ajax/common/__tests__/keyword.spec.ts
+++ src/universal/ajax/common/__tests__/keyword.spec.ts
@@ -1,4 +1,4 @@
-import { LogicalOperators, exactly, exclude, include } from '../keyword'
+import { LogicalOperators, exactly, exclude, group, include, isLogicalOperator } from '../keyword'
 
 describe('Keyword', () => {
 	it('should exactly', () => {
@@ -14,5 +14,20 @@ describe('Keyword', () => {
 	it('should exclude', () => {
 		const phrase = exclude('phrase', 'phrase2')
 		expect(phrase).toEqual('-phrase-phrase2')
+	})
+
+	it('should group', () => {
+		const reduced = group('yay', LogicalOperators.And, 'nay', LogicalOperators.Or, 'ok')
+		expect(reduced).toEqual('yay AND nay OR ok')
+	})
+
+	it('should group with leading operator', () => {
+		const reduced = group(LogicalOperators.Not, 'yay', LogicalOperators.And, 'nay', LogicalOperators.Or, 'ok')
+		expect(reduced).toEqual('yay AND nay OR ok')
+	})
+
+	it('should group with trailing operator', () => {
+		const reduced = group('yay', LogicalOperators.And, 'nay', LogicalOperators.Or)
+		expect(reduced).toEqual('yay AND nay')
 	})
 })
--- src/universal/ajax/common/keyword.ts
+++ src/universal/ajax/common/keyword.ts
@@ -1,16 +1,17 @@
-export type MatchType = {
-	exact?: boolean
-	phrase: string
-}
-
 export enum LogicalOperators {
 	And = 'AND',
 	Or = 'OR',
 	Not = 'NOT'
 }
 
-function prepend(prependchar: string, ...phrases: string[]) {
-	return phrases.map(phrase => `${prependchar}${phrase}`).join('')
+function prepend(prependchar: string, ...phrases: string[]): string {
+	if (phrases == null || phrases.length === 0) return ''
+	const reduced = phrases.reduce((previousValue, currentValue, index) => `${previousValue}${prependchar}${currentValue}`)
+	return `${prependchar}${reduced}`
+}
+
+export function isLogicalOperator(phrase: string | LogicalOperators): phrase is LogicalOperators {
+	return phrase === LogicalOperators.And || phrase === LogicalOperators.Not || phrase === LogicalOperators.Or
 }
 
 export function exactly(phrase: string): string {
@@ -24,4 +25,24 @@ export function include(...phrases: string[]): string {
 
 export function exclude(...phrases: string[]): string {
 	return prepend('-', ...phrases)
+}
+
+export function group(...phrases: Array<string | LogicalOperators>) {
+	if (phrases == null || phrases.length === 0) return ''
+
+	// we get rid of any leading operators
+	if (isLogicalOperator(phrases[0])) {
+		phrases.splice(0, 1)
+	}
+
+	const reduced = phrases
+		.reduce((previousValue, currentValue, index, array) => {
+			// we ignore any trailing operators
+			if (index >= array.length - 1 && isLogicalOperator(currentValue)) {
+				return previousValue
+			}
+			return `${previousValue} ${currentValue}`
+		})
+
+	return reduced
 }
